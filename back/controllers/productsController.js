1d65ef1e62f634b62c7303ffd0762f7c9a995228
Viviana Saenz <vivianasaenz.m@gmail.com>
2022-10-12 22:16:48 -0500

    update 12/10/2022

--- back/controllers/productsController.js
+++ back/controllers/productsController.js
@@ -1,7 +1,19 @@
-//Dentro del metodo getProducts puedo utilizar cualquiera de estas herramientas (Getmapping)
+const producto=require("../models/productos")
+
+//Ver la lista de productos
 exports.getProducts=(req,res,next) =>{
     res.status(200).json({
         success:true,
         message: "En esta ruta usted va a poder ver todos los productos"
+    })
+}
+
+//Crear un nuevo producto /api/productos
+exports.newProduct=async(req,res,next)=>{
+    const product=await producto.create(req.body);
+
+    res.status(201).json({
+        success:true,
+        product
     })
 }
--- back/server.js
+++ back/server.js
@@ -2,7 +2,12 @@ const app=require("./app")
 
 //Setear el archivo de configuración
 const dotenv=require("dotenv");
+const connectDatabase = require("./config/database");
 dotenv.config({path: 'back/config/config.env'})
+
+//Configurar base de datos con el metodo connectDatabase
+
+connectDatabase();
 
 //Llamamos al server
 const server=app.listen(process.env.PORT, () =>{
